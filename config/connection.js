5da9733fcb7a790d7607d9eb3f518e24891f26df
Ruben Chavez <chruben94@gmail.com>
2020-02-28 23:53:05 -0800

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,25 +2,25 @@ var mysql = require("mysql");
 
 var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: "l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//     port: 3306,
-//     user: "um3845gnwiqjhdyg",
-//     password: "vl7akkvir3bx6swu",
-//     database: "be2xabc6u6ett62p"
-//   });
-// }
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "kil9uzd3tgem3naa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "zdaeiu6qnn8kzi75",
+    password: "ws41fv98e3i0q1t3",
+    database: "w6jsjp1vzslvstkh"
+  });
+}
 
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+// connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
 
 connection.connect(function (err) {
   if (err) {

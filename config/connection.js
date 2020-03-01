5137c4a1e20ff0f37b19cf40cad96c6b310723a5
Ruben Chavez <chruben94@gmail.com>
2020-02-29 21:20:56 -0800

    updated

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "kil9uzd3tgem3naa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "zdaeiu6qnn8kzi75",
-    password: "ws41fv98e3i0q1t3",
-    database: "w6jsjp1vzslvstkh"
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgers_db"
   });
 }
 
